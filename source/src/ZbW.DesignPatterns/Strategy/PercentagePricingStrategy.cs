1b29649d250724325378d2491cdce33a4aa022ad
Cédric Menzi <cedric.menzi@buhlergroup.com>
2021-05-10 18:07:22 +0200

    added percentage pricing strategy

---
+++ source/src/ZbW.DesignPatterns/Strategy/PercentagePricingStrategy.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlTypes;
+
+namespace ZbW.DesignPatterns.Strategy
+{
+    public class PercentagePricingStrategy : IPricingStrategy
+    {
+        private readonly decimal _percentage;
+
+        public PercentagePricingStrategy(decimal percentage)
+        {
+            _percentage = percentage;
+        }
+
+        public decimal GetDiscount(Sale sale)
+        {
+            return sale.Amount * _percentage / 100;
+        }
+    }
+}
---
+++ source/tests/ZbW.DesignPatterns.Tests/Strategy/SaleTests.cs
@@ -0,0 +1,30 @@
+﻿namespace ZbW.DesignPatterns.Tests.Strategy
+{
+    using FluentAssertions;
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Xunit;
+
+    using ZbW.DesignPatterns.Strategy;
+
+    public class SaleTests
+    {
+        [Fact]
+        public void GetTotal_WhenTotal100And10PercentDiscount_Then90()
+        {
+            // Arrange
+            var sale = new Sale(100, new PercentagePricingStrategy(10));
+
+            // Act
+            var result = sale.GetTotal();
+
+            // Assert
+            result.Should().Be(90);
+        }
+    }
+}
