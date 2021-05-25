448198f359ec8f39ae3b898abb123e6b1ad406d1
Lukas Heeb <lu.heeb@gmail.com>
2021-05-25 18:37:39 +0200

    bridge pattern added

---
+++ source/src/ZbW.DesignPatterns/Bridge/Cash.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ZbW.DesignPatterns.Bridge
+{
+    public class Cash : IPaymentMethod
+    {
+        public string Pay(decimal amount)
+        {
+            return $"Payed {amount} Fr. with cash";
+        }
+    }
+}
