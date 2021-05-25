10cfbd796525f4dce094952c992287cd2a6fadbc
fabioeichmann <fabio.eichmann@gmail.com>
2021-05-25 19:09:41 +0200

    Add Facade Pattern

---
+++ source/src/ZbW.DesignPatterns/Facade/PizzaStoreSubSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ZbW.DesignPatterns.Facade
+{
+    public class PizzaStoreSubSystem
+    {
+        internal string Order(string[] pizzas)
+        {
+            string orderText = string.Empty;
+            foreach(var pizza in pizzas)
+            {
+                orderText += pizza + ",";
+            }
+
+            return orderText;
+        }
+    }
+}
