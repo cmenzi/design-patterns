276a0015db3231d13ea6cd46f85bb82d8a98be15
marctraber <traber.marc@hispeed.ch>
2021-06-20 09:58:46 +0200

    Add     [Serializable] to Strategy's --> Prototype Pattern

--- source/src/ZbW.DesignPatterns/Strategy/NullDiscountStrategy.cs
+++ source/src/ZbW.DesignPatterns/Strategy/NullDiscountStrategy.cs
@@ -7,6 +7,7 @@
     using System.Text;
     using System.Threading.Tasks;
 
+    [Serializable]
     public class NullDiscountStrategy : IPricingStrategy
     {
         public decimal GetDiscount(Sale sale)
